98f92d36f2d0198f2687bc4974ab079c77706e8c
pleaseDoNotTheCat <85490435+pleaseDoNotTheCat@users.noreply.github.com>
2022-01-04 14:32:21 +0100

    Update say.js

--- komendy/say.js
+++ komendy/say.js
@@ -6,6 +6,7 @@ module.exports = {
         if(!message.member.roles.cache.some(role => role.id === '819280242295701506')) return message.reply('tylko dla cituchów i administracji');
         if (!msg) return message.reply('Po co używasz skoro nie podano tekstu?');
         message.channel.send({ content: `<@${message.author.id}>: ${msg}`, allowedMentions: { user: false }, allowedMentions: { roles: false }, allowedMentions: { everyone: false } });
+	message.delete();
        console.log(`uzyto saya! uzytkownik: ${message.author.tag} ${message.author.id}`);
 	}
 }

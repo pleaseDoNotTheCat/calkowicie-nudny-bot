4b6ea36f875f8e63082b1d2b791a9440a53801ef
pleaseDoNotTheCat <85490435+pleaseDoNotTheCat@users.noreply.github.com>
2022-01-15 17:38:42 +0100

    zmiana evala

--- komendy/eval.js
+++ komendy/eval.js
@@ -1,4 +1,3 @@
-const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "eval",
     category: "Developers",
@@ -7,23 +6,17 @@ module.exports = {
         const { client, author:user, member } = message;
         if (message.author.id !== "831943801818841198")
         if (message.author.id !== "804401104976216105")
+        if (message.author.id !== "847042688675217439")
         if (message.author.id !== "526711537373806592")
         if (message.author.id !== "540984502252601356") return;
         try {
-            const code = args.join(" ");
+            const code = args.join(" ").replaceAll('token', 'nie');
             let valueOut = eval(code);
-    const embed1 = new MessageEmbed()
-     .setTitle(typeof valueOut)
-     .setDescription(`\`\`\`js\n${require('util').inspect(valueOut)}\`\`\` `)
-     .setColor('GREEN')
-     .setFooter('Medal sie należy')
-            message.reply({embeds: [embed1]});
+
+            message.reply(`In: ${code}\nOut: ${require('util').inspect(valueOut)}`);
           } catch (error) {
-            const embed2 = new MessageEmbed()
-.setDescription(` \`\`\`js\nnie bo error: ${error}\`\`\` `)
-.setColor('RED')
-.setFooter('troche jeszcze popracuj')
-            message.reply({embeds: [embed2]});
+              const code = args.join(' ');
+            message.reply(`In: ${code}\nError:${error}`);
         }
     }
 }

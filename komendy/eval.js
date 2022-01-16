ec76347041cbecdc7d93dbcdb9012f7dca098dbe
pleaseDoNotTheCat <85490435+pleaseDoNotTheCat@users.noreply.github.com>
2022-01-16 19:27:07 +0100

    przypadkiem rozwalilem evala, wiec komenda no work

--- komendy/eval.js
+++ komendy/eval.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js')
+/*const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "eval",
     category: "Developers",
@@ -26,3 +26,4 @@ const e = new MessageEmbed()
 }
     }
 }
+/*

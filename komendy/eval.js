1fd77223920fdfdcfb251d4eb28335165b874187
pleaseDoNotTheCat <85490435+pleaseDoNotTheCat@users.noreply.github.com>
2022-01-16 18:54:08 +0100

    poprawiono evala, i usunieto nie potrzebne id

--- komendy/eval.js
+++ komendy/eval.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: "eval",
     category: "Developers",
@@ -5,18 +6,23 @@ module.exports = {
     execute(message, args) {
         const { client, author:user, member } = message;
         if (message.author.id !== "831943801818841198")
+        if (message.author.id !== '508258776134975499')
         if (message.author.id !== "804401104976216105")
         if (message.author.id !== "847042688675217439")
-        if (message.author.id !== "526711537373806592")
         if (message.author.id !== "540984502252601356") return;
         try {
-            const code = args.join(" ").replaceAll('token', 'nie');
+            const code = args.join(" ").replaceAll('token', 'ping');
             let valueOut = eval(code);
-
-            message.reply(`In: ${code}\nOut: ${require('util').inspect(valueOut)}`);
-          } catch (error) {
+const embed = new MessageEmbed()
+.setTitle('Uruchamianie kodu')
+.setDescription(` \`\`\`js\n${require('util').inspect(valueOut)}\`\`\` \nTyp \`\`\`js\n${typeof valueOut}\`\`\`  `)
+.setColor('GREEN')
+				} catch (error) {
               const code = args.join(' ');
-            message.reply(`In: ${code}\nError:${error}`);
-        }
+const e = new MessageEmbed()
+.setTitle('Uruchamianie kodu')
+.setDescription(` \`\`\`js\n${error}\`\`\` `) 
+.setColor('RED')
+}
     }
 }
